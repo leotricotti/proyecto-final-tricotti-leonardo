d444cc5fb2747b09ce321a4b0c10243cbffae902
Leonardo Tricotti <tricottileo@gmail.com>
2022-09-02 11:47:00 -0300

    pagos js terminado

--- js/pagos.js
+++ js/pagos.js
@@ -1,8 +1,10 @@
-//Funcion que inyecta la tabla con la informacion de los servicios por vencer 
+//Declaracion de variable que va a almacenar los servicios por vencer que provienen del json
+let serviciosLocalStorage;
+//Funcion que inyecta la tabla con la informacion de los servicios por vencer
 const mostrarServicios = () => {
   //Codigo que recupera los servicios a pagar almacenados en el localstorage
   serviciosLocalStorage = JSON.parse(localStorage.getItem("servicios"));
-  //Código que crea el elemento tabla y le asigna sus clases de bootstrap
+  //Código que crea el elemento tabla y le asigna clases de bootstrap
   let table = document.createElement("table");
   table.className = "table table-hover";
   //Código que crea la cabeza de la tabla
@@ -18,9 +20,9 @@ const mostrarServicios = () => {
         </thead>
     `;
   //Codigo que crea el cuerpo de la tabla y agrega el divisor entre cabeza y cuerpo
-  let tableBody = document.createElement("tbody");
+  tableBody = document.createElement("tbody");
   tableBody.className = "table-group-divider";
-  //Codigo que recorre el array de cuentas creado anteriormente y asigna cada elemento a su culumna
+  //Codigo que crea el cuerpo de la tabla
   for (const servicio of serviciosLocalStorage) {
     tableBody.innerHTML += `
         <tr>
@@ -37,7 +39,7 @@ const mostrarServicios = () => {
   //Codigo que asigna a un padre la tabla creada anteriormente
   tableContainer = document.querySelector(".table-container");
   tableContainer.append(table);
-}
+};
 //Llamada a la funcion que inyecta al html la tabla con las cuentas habilitadas
 mostrarServicios();
 //Codigo que captura el campo input
@@ -47,8 +49,8 @@ let capturarValor = document.getElementById("pagos-submit");
 //Operador que desestructura el array de objetos
 const [a, b, c, d, e] = serviciosLocalStorage;
 //Funcion que captura la cuenta seleccionada y devuelve un campo para ingresar el importe que se desea transferir
-const seleccionarServicio = (inputValue) =>{
-  if(inputValue == "01"){
+const seleccionarServicio = (inputValue) => {
+  if (inputValue == "01") {
     //Codigo que quita la tabla con las cuentas habilitadas
     tableContainer.innerHTML = "";
     //Variable que almacena el resultado de la operacion
@@ -57,7 +59,7 @@ const seleccionarServicio = (inputValue) =>{
     comprobarSaldo(a.servicio, a.numero);
     //Funcion que retorna el monto abonado como componente del objeto operacion
     cargarMonto = () => a.numero;
-  }else if(inputValue == "02"){
+  } else if (inputValue == "02") {
     //Codigo que quita la tabla con las cuentas habilitadas
     tableContainer.innerHTML = "";
     //Variable que almacena el resultado de la operacion
@@ -66,7 +68,7 @@ const seleccionarServicio = (inputValue) =>{
     comprobarSaldo(b.servicio, b.numero);
     //Funcion que retorna el monto abonado como componente del objeto operacion
     cargarMonto = () => b.numero;
-  }else if(inputValue == "03"){
+  } else if (inputValue == "03") {
     //Codigo que quita la tabla con las cuentas habilitadas
     tableContainer.innerHTML = "";
     //Variable que almacena el resultado de la operacion
@@ -75,7 +77,7 @@ const seleccionarServicio = (inputValue) =>{
     comprobarSaldo(c.servicio, c.numero);
     //Funcion que retorna el monto abonado como componente del objeto operacion
     cargarMonto = () => c.numero;
-  }else if(inputValue == "04"){
+  } else if (inputValue == "04") {
     //Codigo que quita la tabla con las cuentas habilitadas
     tableContainer.innerHTML = "";
     //Variable que almacena el resultado de la operacion
@@ -84,7 +86,7 @@ const seleccionarServicio = (inputValue) =>{
     comprobarSaldo(d.servicio, d.numero);
     //Funcion que retorna el monto abonado como componente del objeto operacion
     cargarMonto = () => d.numero;
-  }else if(inputValue == "05"){
+  } else if (inputValue == "05") {
     //Codigo que quita la tabla con las cuentas habilitadas
     tableContainer.innerHTML = "";
     //Variable que almacena el resultado de la operacion
@@ -93,53 +95,56 @@ const seleccionarServicio = (inputValue) =>{
     comprobarSaldo(e.servicio, e.numero);
     //Funcion que retorna el monto abonado como componente del objeto operacion
     cargarMonto = () => e.numero;
-  }else{
+  } else {
     Swal.fire({
-      icon: 'warning',
-      title: 'Ingrese una opción valida',
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'Aceptar',
+      icon: "warning",
+      title: "Ingrese una opción valida",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Aceptar",
       showClass: {
-        popup: 'animate__animated animate__fadeIn'
-      }
+        popup: "animate__animated animate__fadeIn",
+      },
     });
   }
-}
+};
 //Funcion que confirma la operacion via un alert
 const confirmarOperacion = (a, b) => {
   Swal.fire({
     icon: "question",
-    title: `Desea pagar el servicio ${a} por el importe de ${numeroADinero(b)} ?`,
-    confirmButtonText: 'Save',
+    title: `Desea pagar el servicio ${a} por el importe de ${numeroADinero(
+      b
+    )} ?`,
+    confirmButtonText: "Save",
     confirmButtonColor: "#3085d6",
     confirmButtonText: "Aceptar",
     showCancelButton: true,
     cancelButtonText: "Cancelar",
     showClass: {
       popup: "animate__animated animate__fadeIn",
-    }
+    },
   }).then((result) => {
     if (result.isConfirmed) {
       Swal.fire(
-        `Operación realizada con éxito. Su saldo es ${numeroADinero(actualizarSaldoCajaAhorro())}`
+        `Operación realizada con éxito. Su saldo es ${numeroADinero(
+          actualizarSaldoCajaAhorro()
+        )}`
       ).then(function () {
-        window.location.href = "../opcion/opcion.html";
         actualizarSaldoCajaAhorro();
         actualizarSaldoStorage();
         crearOperacion();
         cargarOperacion();
-      })
+        enviarDatos();
+        // window.location.href = "../opcion/opcion.html";
+      });
     } else if (result.isDismissed) {
-      Swal.fire(
-        'Operación Cancelada', '', 'error'
-      ).then(function () {
+      Swal.fire("Operación Cancelada", "", "error").then(function () {
         window.location.href = "../opcion/opcion.html";
-      })
+      });
     }
-  })
-}
+  });
+};
 //Funcion que captura la seleccion realizada por el usuario
-capturarValor.addEventListener('click', function() {
+capturarValor.addEventListener("click", function () {
   seleccionarServicio(pagosInput.value);
 });
 //Codigo que captura el boton que modifica la operacion
@@ -147,30 +152,27 @@ const clean = document.getElementById("limpiar-campo");
 // Funcion que limpia el campo input en caso de que el usuario quiera modificar el importe a depositar
 clean.onclick = () => {
   pagosInput.value = "";
-}
+};
 //Funcion que confirma que el usuario tenga fondos suficientes antes de realizar la operacion
 const comprobarSaldo = (a, b) => {
-  if(saldoCajaAhorro <= 0){     
+  if (saldoCajaAhorro <= 0) {
     Swal.fire({
-    icon: 'warning',
-    title: 'Saldo Insuficiente',
-    confirmButtonColor: '#3085d6',
-    confirmButtonText: 'Aceptar',
-    showClass: {
-      popup: 'animate__animated animate__fadeIn'
-    }
-  }).then(() => {
-      Swal.fire(
-        'Operación Cancelada', '', 'error'
-      ).then(function () {
+      icon: "warning",
+      title: "Saldo Insuficiente",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Aceptar",
+      showClass: {
+        popup: "animate__animated animate__fadeIn",
+      },
+    }).then(() => {
+      Swal.fire("Operación Cancelada", "", "error").then(function () {
         window.location.href = "../opcion/opcion.html";
-      })
-    })
+      });
+    });
   } else {
     confirmarOperacion(a, b);
   }
-}
-
+};
 // Constructor del objeto operaciones
 class Operacion {
   constructor(fecha, hora, operacion, monto, saldo) {
@@ -180,9 +182,14 @@ class Operacion {
     this.monto = monto;
     this.saldo = saldo;
   }
-}
+};
 //Codigo que informa el tipo de operacion
 const nombrarOperacion = () => "Pago Servicio";
+//Funcion que captura la fecha en que se realiza la operación
+const capturarDia = () => new Date().toLocaleDateString();
+//Funcion que captura la hora en que se realiza la operacion
+const capturarHora = () => new Date().toLocaleTimeString();
+//Funcion que crea la nueva operacion
 const crearOperacion = () => {
   nuevaOperacion = new Operacion(
     capturarDia(),
@@ -192,14 +199,23 @@ const crearOperacion = () => {
     numeroADinero(actualizarSaldoCajaAhorro())
   );
   return nuevaOperacion;
-}
-const serviciosPagados = [];
-//Codigo que crea la variable donde se almacenaran las operaciones simuladas
-let serviciosPagadosLocalStorage = (localStorage.getItem("serviciosPagados"));
-//Operador avanzado que verifica si existe en el localstorage el objeto operaciones dolar, si no es así lo crea
-serviciosPagadosLocalStorage == null && localStorage.setItem("serviciosPagados", serviciosPagados);
-//Funcion que almacena las nuevas operaciones en el localstorage
+};
+//Funcion que almacena la nueva operaciones en una variable para luego ser enviada al servidor
 const cargarOperacion = () => {
-  serviciosPagados.unshift(crearOperacion());
-  guardarLocal("serviciosPagados", JSON.stringify(serviciosPagados));
-}
+  serviciosPagados = crearOperacion();
+};
+//Funcion que envia la informacion generada por el usuario al servidor y la almacena en el localstorage
+const enviarDatos = () => {
+  const jsonplaceholder = "https://jsonplaceholder.typicode.com/posts";
+  fetch(jsonplaceholder, {
+    method: "POST",
+    body: JSON.stringify(serviciosPagados),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      localStorage.setItem("servicioPagado", JSON.stringify(datos));
+    });
+};
