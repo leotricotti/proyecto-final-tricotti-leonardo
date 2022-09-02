613f81e758690fa79f2ea532eb405af35e2ee061
Leonardo Tricotti <tricottileo@gmail.com>
2022-09-02 20:32:07 -0300

    nikkei agregado

--- js/inversiones.js
+++ js/inversiones.js
@@ -11,19 +11,6 @@
 // 	.then(response => console.log(response[1].price.previous_close))
 // 	.catch(err => console.error(err));
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '0fc78676efmsh63b6bb59f51558fp1d14fbjsn748e1845fb4f',
-// 		'X-RapidAPI-Host': 'google-finance4.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://google-finance4.p.rapidapi.com/search/?q=ftse&hl=en&gl=US', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
 const options = {
 	method: 'GET',
 	headers: {
@@ -32,7 +19,92 @@ const options = {
 	}
 };
 
-fetch('https://google-finance4.p.rapidapi.com/search/?q=nikkei&hl=en&gl=US', options)
+fetch('https://google-finance4.p.rapidapi.com/search/?q=ftse&hl=en&gl=US', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
+const btnNikkei = document.getElementById("nikkei");
+const getDatosNikkei = function () {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "0fc78676efmsh63b6bb59f51558fp1d14fbjsn748e1845fb4f",
+      "X-RapidAPI-Host": "google-finance4.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    "https://google-finance4.p.rapidapi.com/search/?q=nikkei&hl=en&gl=US",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      //Codigo que cambia el subtitulo del simulador
+      let text = document.querySelector(".text");
+      text.innerText = "Indice NIKKEI";
+      //Código que crea el elemento tabla y le asigna sus clases
+      let table = document.createElement("table");
+      table.className = "table table-hover";
+      //Código que crea la cabeza de la tabla
+      let tableHead = document.createElement("thead");
+      tableHead.innerHTML = `
+      <thead>
+        <tr>
+          <th scope="col">Título</th>
+          <th scope="col">Apertura</th>
+          <th scope="col">Variacion</th>
+          <th scope="col">Porcentaje</th>
+          <th scope="col">Cierre</th>
+        </tr>
+      </thead>
+    `;
+      //Codigo que crea el cuerpo de la tabla y agrega el divisor entre cabeza y cuerpo
+      let tableBody = document.createElement("tbody");
+      tableBody.className = "table-group-divider";
+      //Codigo que recorre el array de cuentas creado anteriormente y asigna cada elemento a su columna
+      for (const dato of response) {
+        tableBody.innerHTML += `
+        <tr>
+          <td>${dato.info.ticker_symbols[0]}</td>
+          <td>${formatearPrecio(dato.price.previous_close)}</td>
+          <td>${quitarDecimales(dato.price.last.today_change)}</td>
+          <td>${quitarDecimales(dato.price.last.today_change_percent)}</td>
+          <td>${formatearPrecio(dato.price.previous_close)}</td>
+        </tr>
+      `;
+      }
+      //Codigo que agrega la cabeza y el cuerpo a la tabla creada anteriormente
+      table.append(tableHead);
+      table.append(tableBody);
+      //Codigo que asigna a un padre la tabla creada anteriormente
+      let tableContainer = document.querySelector(".table-container");
+      tableContainer.append(table);
+    })
+    .catch((err) => console.error(err));
+};
+
+btnNikkei.onclick = () => {
+  getDatosNikkei();
+};
+
+//Funcion que da formato de dinero al dato recibido
+function formatearPrecio(precio){
+  if(precio == null){
+    precio = "S/C";
+  }else{
+    precio = numeroADinero(precio);
+  }
+  return precio;
+}; 
+
+const quitarDecimales = (num) => {
+  if(num == 0){
+    num = "S/C";
+  }else{
+    num = parseFloat(num).toFixed(2);
+  }
+  return num;
+}
+  
