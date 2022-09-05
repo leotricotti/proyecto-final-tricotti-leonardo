84684a3fb884503a30badf4f46c7c0657866c152
Leonardo Tricotti <tricottileo@gmail.com>
2022-09-05 20:45:33 -0300

    autonumerics agregado

--- js/extracciones.js
+++ js/extracciones.js
@@ -8,6 +8,24 @@ const capturarDiaExtraccion = () => new Date().toLocaleDateString();
 const capturarHoraExtraccion = () => new Date().toLocaleTimeString();
 //Codigo que informa el tipo de operacion
 const nombrarOperacion = () => "Extraccion Adelanto";
+//Codigo que captura el boton que confirma la operacion
+const captura = document.getElementById("extracciones-submit");
+//Funcion que captura la informacion brindada por el usuario y la convierte en un objeto
+//Funcion que separa el miles el numero ingresado por el usuario
+const formatearNumero = () => new AutoNumeric('#extracciones-input', {
+  decimalCharacter : ',',
+  digitGroupSeparator : '.',
+});
+//Llamada a la funcion
+formatearNumero();
+//Declaracion de la variable que va a almacenar el importe ingresado por el usuario
+let unformatNumber;
+captura.onclick = () => {
+  //Asignacion del valor a la variable creada anteriormente(remueve puntos y comas y divide por 100 para remover los decimales) 
+  unformatNumber = inputExtraccion.value.split( /\.|\,/).join("") / 100;  
+  //Llamada a las funciones declaradas
+  comprobarSaldo();
+};
 // Constructor del objeto depositos;
 class Operacion {
   constructor(fecha, hora, operacion, monto, saldo) {
@@ -24,7 +42,7 @@ const crearOperacion = () => {
     capturarDiaExtraccion(),
     capturarHoraExtraccion(),
     nombrarOperacion(),
-    numeroADinero(inputExtraccion.value),
+    numeroADinero(unformatNumber),
     numeroADinero(saldoCajaAhorro)
   );
   return nuevaOperacion;
@@ -38,7 +56,7 @@ const text = document.querySelector(".text");
 const confirmarOperacion = () => {
   Swal.fire({
     icon: "question",
-    title: `Desea extraer la suma de ${numeroADinero(inputExtraccion.value)} ?`,
+    title: `Desea extraer la suma de ${numeroADinero(unformatNumber)} ?`,
     confirmButtonText: "Save",
     confirmButtonColor: "#3085d6",
     confirmButtonText: "Aceptar",
@@ -85,7 +103,7 @@ const confirmarOperacion = () => {
 //Funcion que confirma que el usuario tenga fondos suficientes antes de realizar la operacion
 const comprobarSaldo = () => {
   //Funcion que actualiza el saldo si los fondos son suficientes
-  saldoCajaAhorro -= parseFloat(inputExtraccion.value);
+  saldoCajaAhorro -= unformatNumber;
   if (saldoCajaAhorro <= 0) {
     //Codigo que evita que el saldo se actualice si el saldo es menor a 0S
     saldoCajaAhorro = localStorage.getItem("saldo");
@@ -114,13 +132,6 @@ const comprobarSaldo = () => {
     //Llamada a la funcion
     confirmarOperacion();
   }
-};
-//Codigo que captura el boton que confirma la operacion
-const captura = document.getElementById("extracciones-submit");
-//Funcion que captura la informacion brindada por el usuario y la convierte en un objeto
-captura.onclick = () => {
-  //Llamada a las funciones declaradas
-  comprobarSaldo();
 };
 //Funcion que almacena la nueva operaciones en una variable para luego ser enviada al servidor
 const cargarOperacion = () => {
